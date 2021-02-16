100b2c3262b7568d181897167d3d10ccb634a942
dnguddl9436 <dnguddl9696@gmail.com>
2021-02-16 18:12:08 +0900

    210216

--- dev_java80/src/variable/step1/Quiz1_1.java
+++ dev_java80/src/variable/step1/Quiz1_1.java
@@ -3,7 +3,7 @@ package variable.step1;
 import java.util.Scanner;
 
 public class Quiz1_1 {
-
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("몸무게를 입력하세요: ");
